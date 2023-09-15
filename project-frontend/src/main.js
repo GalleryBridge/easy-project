f8ba1e0a387417e66f23b7d5a736af5f9cf3a94f
jimingzhe521 <83235072@qq.com>
2023-09-15 22:08:10 +0800

    init project

--- project-frontend/src/main.js
+++ project-frontend/src/main.js
@@ -1,14 +1,4 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
 
-Vue.config.productionTip = false
-
-Vue.use(ElementUI, {size: 'small'})
-
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+createApp(App).mount('#app')
