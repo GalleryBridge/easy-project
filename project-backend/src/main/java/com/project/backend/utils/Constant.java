9eb346fd4e211039caae924930e60f1daa98275b
jimingzhe521 <83235072@qq.com>
2023-09-18 18:38:41 +0800

    starter project

---
+++ project-backend/src/main/java/com/project/backend/utils/Constant.java
@@ -0,0 +1,31 @@
+package com.project.backend.utils;
+
+/**
+ * 用户常量
+ */
+
+public interface  Constant {
+    /**
+     * 用户登录态键
+     */
+    public static final String USER_LOGIN_STATE = "user_login";
+
+    //  region 权限
+
+    /**
+     * 默认角色
+     */
+    public static final String DEFAULT_ROLE = "user";
+
+    /**
+     * 管理员角色
+     */
+    public static final String ADMIN_ROLE = "admin";
+
+    /**
+     * 被封号
+     */
+    public static final String BAN_ROLE = "ban";
+
+    // endregion
+}
