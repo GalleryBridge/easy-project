fc88c6907005274ef52fec4756c4ae218102774a
jimingzhe521 <83235072@qq.com>
2023-09-20 11:03:51 +0800

    add a file

--- project-backend/src/main/java/com/project/backend/mapper/AccountMapper.java
+++
@@ -1,9 +0,0 @@
-package com.project.backend.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.project.backend.entity.po.Account;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface AccountMapper extends BaseMapper<Account> {
-}
